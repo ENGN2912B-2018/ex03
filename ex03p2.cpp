06478724ed6785860fc662cc62ae24c7a4caba24
Jason Gaudette <jason_gaudette@brown.edu>
2018-09-12 17:15:57 -0400

    split file into 2

--- ex03p2.cpp
+++ ex03p2.cpp
@@ -20,6 +20,7 @@ int main(int nargc, char* argv[])
     // this modifies the standard output format
     cout.precision(8);
     //cout << setprecision(8); // alternative (requires <iomanip>)
+    cout.width(8);
 
     for (int n=1; n<nargc; n++){
 
@@ -28,22 +29,27 @@ int main(int nargc, char* argv[])
         // echo input value
         cout << endl << string(40,'*') << endl << "v = " << v << endl;
 
-        // compute ln(v) - natural log
-        cout << "ln(v)       = " << log(v) << endl;
+        // the following commands may only be performed on positive values
+        if (v >= 0) {
+            // compute ln(v) - natural log
+            cout << "ln(v)       = " << log(v) << endl;
 
-        // compute log10(v)
-        cout << "log10(v)    = " << log10(v) << endl;
+            // compute log10(v)
+            cout << "log10(v)    = " << log10(v) << endl;
 
-        // compute log2(v)
-        cout << "log2(v)     = " << log2(v) << endl;
+            // compute log2(v)
+            cout << "log2(v)     = " << log2(v) << endl;
 
-        // compute ln(v)/ln(2)
-        cout << "ln(v)/ln(2) = " << (log(v)/log(2)) << endl;
+            // compute ln(v)/ln(2)
+            cout << "ln(v)/ln(2) = " << (log(v)/log(2)) << endl;
+        } else {
+            sprintf("Warning:  Value entered (%f) was negative!\n", v)
+        }
 
         // compute double log2(|v|)
         cout << "log2(|v|)   = " << logb(v) << endl;
 
-        // compute integer floor(log2(|v|))
+        // compute integer log2(|v|)
         cout << "ilog2(|v|)  = " << ilogb(v) << endl;
 
         // compute 2^vlog
